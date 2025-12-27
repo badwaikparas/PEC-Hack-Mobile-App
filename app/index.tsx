69a2b71e144da5ffe4003a304c1946860a4b6678
Tavrez1 <tnehal786@gmail.com>
2025-12-27 10:42:34 +0530

    FoodAnalayzer implemented

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import FoodAnalyzer from '@/services/foodAnalyzer'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello World!</Text>
+      <FoodAnalyzer />
     </View>
   );
 }
